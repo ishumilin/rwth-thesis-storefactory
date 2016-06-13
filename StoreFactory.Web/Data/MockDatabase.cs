f7b0d7a877e58448bccab423e14bb05bda600acd
ishumilin <i@shumil.in>
2016-06-13 16:00:10 +0200

    Seeded database with Wool data (Table 5.3)

--- StoreFactory.Web/Data/MockDatabase.cs
+++ StoreFactory.Web/Data/MockDatabase.cs
@@ -24,6 +24,11 @@ namespace StoreFactory.Web.Data
             ShrinkageParameters.Add(new ShrinkageData { ProductId = 1, MaterialId = 1, Temperature = 180, DwellTime = 5.5, LengthFactor = 0.99, WidthFactor = 0.99, SleeveFactor = 0.99 });
             ShrinkageParameters.Add(new ShrinkageData { ProductId = 1, MaterialId = 1, Temperature = 190, DwellTime = 2.5, LengthFactor = 0.98, WidthFactor = 0.98, SleeveFactor = 0.98 });
             ShrinkageParameters.Add(new ShrinkageData { ProductId = 1, MaterialId = 1, Temperature = 195, DwellTime = 1.0, LengthFactor = 0.97, WidthFactor = 0.97, SleeveFactor = 0.97 });
+
+            // Seed Shrinkage Data - Wool (Table 5.3)
+            // Series 2: Wool, Red/White/Black
+            ShrinkageParameters.Add(new ShrinkageData { ProductId = 1, MaterialId = 2, Temperature = 30, DwellTime = 10, LengthFactor = 1.04, WidthFactor = 0.94, SleeveFactor = 0.96 });
+            ShrinkageParameters.Add(new ShrinkageData { ProductId = 1, MaterialId = 2, Temperature = 30, DwellTime = 20, LengthFactor = 1.03, WidthFactor = 0.95, SleeveFactor = 0.95 });
         }
     }
 }
