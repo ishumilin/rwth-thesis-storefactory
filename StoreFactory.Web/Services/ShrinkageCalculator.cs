b67e3a929d3ecd33e47870dd0ecc4357033bc34e
ishumilin <i@shumil.in>
2016-06-24 21:50:30 +0200

    Added ShrinkageCalculator implementation

--- StoreFactory.Web/Services/ShrinkageCalculator.cs
+++ StoreFactory.Web/Services/ShrinkageCalculator.cs
@@ -9,7 +9,42 @@ namespace StoreFactory.Web.Services
     {
         public ShrinkageData Calculate(int materialId, double temperature, double dwellTime)
         {
-            throw new NotImplementedException();
+            // Simple approach: Filter by Material, then find closest points by Temperature
+            var dataPoints = MockDatabase.ShrinkageParameters
+                .Where(p => p.MaterialId == materialId)
+                .OrderBy(p => p.Temperature)
+                .ToList();
+
+            if (!dataPoints.Any())
+            {
+                // Default no shrinkage
+                return new ShrinkageData { LengthFactor = 1, WidthFactor = 1, SleeveFactor = 1 };
+            }
+
+            // Exact match?
+            var exact = dataPoints.FirstOrDefault(p => p.Temperature == temperature);
+            if (exact != null) return exact;
+
+            // Interpolate
+            var lower = dataPoints.LastOrDefault(p => p.Temperature < temperature);
+            var upper = dataPoints.FirstOrDefault(p => p.Temperature > temperature);
+
+            if (lower == null) return upper; // Too low, clamp to min
+            if (upper == null) return lower; // Too high, clamp to max
+
+            // Interpolate Factors
+            var result = new ShrinkageData
+            {
+                ProductId = lower.ProductId,
+                MaterialId = materialId,
+                Temperature = temperature,
+                DwellTime = dwellTime,
+                LengthFactor = Interpolate(temperature, lower.Temperature, upper.Temperature, lower.LengthFactor, upper.LengthFactor),
+                WidthFactor = Interpolate(temperature, lower.Temperature, upper.Temperature, lower.WidthFactor, upper.WidthFactor),
+                SleeveFactor = Interpolate(temperature, lower.Temperature, upper.Temperature, lower.SleeveFactor, upper.SleeveFactor)
+            };
+
+            return result;
         }
 
         private double Interpolate(double x, double x0, double x1, double y0, double y1)
