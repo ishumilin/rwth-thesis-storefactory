32d6859e816efc9df7b30b4f0907d92cda2e7eb5
ishumilin <i@shumil.in>
2016-10-08 10:20:00 +0200

    Add simulator recommendation API

--- StoreFactory.Web/Models/SimulatorDtos.cs
+++ StoreFactory.Web/Models/SimulatorDtos.cs
@@ -68,6 +68,58 @@ namespace StoreFactory.Web.Models
         public ShrinkageData Result { get; set; }
     }
 
+    // Smart-factory style recommendation based on the existing shrinkage dataset.
+    // We do not train a new model; we simply scan a (T,D) grid and rank feasible points.
+    public class RecommendRequest
+    {
+        public int MaterialId { get; set; }
+
+        // throughput|energy|target
+        public string Mode { get; set; } = "throughput";
+
+        // Quality constraints (minimum acceptable factors)
+        public double MinLengthFactor { get; set; } = 0.90;
+        public double MinWidthFactor { get; set; } = 0.90;
+        public double MinSleeveFactor { get; set; } = 0.90;
+
+        // Optional process constraints
+        public double? MaxTemperature { get; set; }
+        public double? MaxDwellTime { get; set; }
+
+        // Targets (used when Mode == target)
+        public double TargetLengthFactor { get; set; } = 0.95;
+        public double TargetWidthFactor { get; set; } = 0.95;
+        public double TargetSleeveFactor { get; set; } = 0.95;
+
+        // Grid scan resolution
+        public int StartTemperature { get; set; } = 30;
+        public int EndTemperature { get; set; } = 200;
+        public int StepTemperature { get; set; } = 5;
+
+        public double StartDwellTime { get; set; } = 1;
+        public double EndDwellTime { get; set; } = 20;
+        public double StepDwellTime { get; set; } = 0.5;
+
+        public int TopN { get; set; } = 3;
+    }
+
+    public class RecommendCandidate
+    {
+        public double Temperature { get; set; }
+        public double DwellTime { get; set; }
+        public double Score { get; set; }
+        public ShrinkageData Result { get; set; }
+        public string Label { get; set; } // e.g., "Best", "Fastest", "Safest"
+    }
+
+    public class RecommendResponse
+    {
+        public string Mode { get; set; }
+        public List<RecommendCandidate> Candidates { get; set; } = new List<RecommendCandidate>();
+        public int EvaluatedPoints { get; set; }
+        public int FeasiblePoints { get; set; }
+    }
+
     public class ExperimentEntry
     {
         public long UnixMs { get; set; }
